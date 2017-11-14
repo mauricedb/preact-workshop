e737bc1db969a5ad7e046205f9b5c857c0a1b22a
Maurice de Beijer <maurice.de.beijer@gmail.com>
2017-11-14 10:35:02 +0100

    Show a list of movies

--- src/routes/movies/index.js
+++ src/routes/movies/index.js
@@ -1,11 +1,22 @@
 import { h, Component } from "preact";
-import style from './style';
+import style from "./style";
 
 class Movies extends Component {
-  render() {
+  state = {
+    movies: []
+  };
+
+  componentDidMount() {
+    fetch("/api/movies.json")
+      .then(rsp => rsp.json())
+      .then(movies => this.setState({ movies }));
+  }
+
+  render({}, { movies }) {
     return (
       <div class={style.movies}>
         <h2>Movies</h2>
+        <ul>{movies.map(movie => <li>{movie.title}</li>)}</ul>
       </div>
     );
   }
