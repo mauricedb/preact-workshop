b09995772092fb540c9c1fe391373af24737b2ed
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-05-20 17:02:13 +0200

    Async await only works during development and not when deploying

--- src/routes/movie/movie-container.js
+++ src/routes/movie/movie-container.js
@@ -14,23 +14,23 @@ export default class MovieContainer extends Component {
     alert('Saving ' + this.state.movie.title);
   };
 
-  // componentDidMount() {
-  //   const id = +this.props.id;
-
-  //   fetch("/api/movies.json")
-  //     .then(rsp => rsp.json())
-  //     .then(movies => movies.find(m => m.id === id))
-  //     .then(movie => this.setState({ movie }));
-  // }
-
-  async componentDidMount() {
+  componentDidMount() {
     const id = +this.props.id;
 
-    const rsp = await fetch('/api/movies.json');
-    const movies = await rsp.json();
-    const movie = movies.find(m => m.id === id);
-    this.setState({ movie });
+    fetch("/api/movies.json")
+      .then(rsp => rsp.json())
+      .then(movies => movies.find(m => m.id === id))
+      .then(movie => this.setState({ movie }));
   }
+
+  // async componentDidMount() {
+  //   const id = +this.props.id;
+
+  //   const rsp = await fetch('/api/movies.json');
+  //   const movies = await rsp.json();
+  //   const movie = movies.find(m => m.id === id);
+  //   this.setState({ movie });
+  // }
 
   render(props, { movie }) {
     if (!movie) return null;
